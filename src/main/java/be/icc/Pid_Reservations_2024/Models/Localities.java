a695bf99be633f1be3111f6975743adb5dac2c43
Brahms <bramso.brahms@gmail.com>
2025-01-07 22:18:15 +0100

    Create Model, Repository, Service and Service

---
+++ src/main/java/be/icc/Pid_Reservations_2024/Models/Localities.java
@@ -0,0 +1,43 @@
+package be.icc.Pid_Reservations_2024.Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data @NoArgsConstructor
+@Entity
+@Getter @Setter
+@Table(name = "Localities")
+public class Localities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "postal_code", length = 6)
+    private String postalCode;
+    @Column(name = "locality", length = 60)
+    private String locality;
+
+    // Relation One To Many
+    //@OneToMany(mappedBy = "localities")
+    //private List<Locations> locations;
+
+    // Constructor with params
+    public Localities(String postal_code, String locality) {
+        this.postalCode = postal_code;
+        this.locality = locality;
+    }
+
+    // toString for locality and postalCode
+    @Override
+    public String toString() {
+        return "Localities{" +
+                "postal_code='" + postalCode + '\'' +
+                ", locality='" + locality + '\'' +
+                '}';
+    }
+}
