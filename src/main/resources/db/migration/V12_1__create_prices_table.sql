072f8219226a228485fd49381baa044cc423cbf4
Brahms <bramso.brahms@gmail.com>
2025-01-08 00:46:29 +0100

    Add Layouts

--- src/main/resources/db/migration/V12_1__create_prices_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS prices (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    end_date DATE,
-    price FLOAT NOT NULL,
-    start_date DATE,
-    type VARCHAR(30),
-    PRIMARY KEY (id)
-);
