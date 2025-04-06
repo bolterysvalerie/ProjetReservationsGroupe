5e339b018869a2924f9670d9f7f3b59529aa6953
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-04-06 15:11:37 +0200

    MUpdate nom LocalityRepository et LocalityService

--- src/main/java/be/icc/Pid_Reservations_2024/Repositories/LocalityRepository.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Repositories/LocalityRepository.java
@@ -3,7 +3,7 @@ package be.icc.Pid_Reservations_2024.Repositories;
 import be.icc.Pid_Reservations_2024.Models.Locality;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LocalitiesRepository extends CrudRepository<Locality, Long> {
+public interface LocalityRepository extends CrudRepository<Locality, Long> {
 
     Locality findByPostalCode(String postalCode);
 
