3a3a8136f41060a1312cf91c73ea154105524481
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-04-08 16:33:00 +0200

    ajout de méthodes addRepresentation, removeRepresentation, addArtistType, removeArtistType, getArtists

--- src/main/java/be/icc/Pid_Reservations_2024/Models/Show.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Models/Show.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -60,6 +61,82 @@ public class Show {
         this.posterUrl = posterUrl;
         this.created_in = created_in;
         this.bookable = bookable;
+    }
+
+    //Ajout jusqu'au toString
+    public Show addRepresentation(Representation representation) {
+        if(!this.representations.contains(representation)) {
+            this.representations.add(representation);
+            representation.setShow(this);
+        }
+
+        return this;
+    }
+
+    public Show removeRepresentation(Representation representation) {
+        if(this.representations.contains(representation)) {
+            this.representations.remove(representation);
+            if(representation.getLocation().equals(this)) {
+                representation.setLocation(null);
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     * Get the performances (artists in a type of collaboration) for the show
+     */
+    public List<ArtisteType> getArtistTypes() {
+        return artiste_types;
+    }
+
+    public Show addArtistType(ArtisteType artistType) {
+        if(!this.artiste_types.contains(artistType)) {
+            this.artiste_types.add(artistType);
+            artistType.addShow(this);
+        }
+
+        return this;
+    }
+
+    public Show removeArtistType(ArtisteType artistType) {
+        if(this.artiste_types.contains(artistType)) {
+            this.artiste_types.remove(artistType);
+            artistType.getShows().remove(this);
+        }
+
+        return this;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public Show addReview(Review review) {
+        if (!reviews.contains(review)) {
+            reviews.add(review);
+            review.setShow(this);
+        }
+        return this;
+    }
+
+    public Show removeReview(Review review) {
+        if (reviews.contains(review)) {
+            reviews.remove(review);
+            review.setShow(null);
+        }
+        return this;
+    }
+
+    public List<Artist> getArtists() {
+        List<Artist> artists = new ArrayList<>();
+        for (ArtisteType artistType : this.artiste_types) {
+            if (artistType.getArtist() != null) {
+                artists.add(artistType.getArtist());
+            }
+        }
+        return artists;
     }
 
     // ToString
