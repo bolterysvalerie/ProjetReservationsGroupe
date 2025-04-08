fe644d0bbd54bb197c973a36bb54b6b20722e128
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-04-08 16:38:58 +0200

    update ShowRepository et modifications de update et delete dans ShowService

--- src/main/java/be/icc/Pid_Reservations_2024/Repositories/ShowRepository.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Repositories/ShowRepository.java
@@ -1,11 +1,16 @@
 package be.icc.Pid_Reservations_2024.Repositories;
 
+import be.icc.Pid_Reservations_2024.Models.Location;
 import be.icc.Pid_Reservations_2024.Models.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ShowRepository extends JpaRepository<Show, Integer> {
 
     Optional<Show> findById(long id);
+    Show findBySlug(String slug);
+    Show findByTitle(String title);
+    List<Show> findByLocation(Location location);
 }
--- src/main/java/be/icc/Pid_Reservations_2024/Services/ShowService.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Services/ShowService.java
@@ -1,11 +1,19 @@
 package be.icc.Pid_Reservations_2024.Services;
 
+import be.icc.Pid_Reservations_2024.Models.ArtisteType;
+import be.icc.Pid_Reservations_2024.Models.Location;
 import be.icc.Pid_Reservations_2024.Models.Show;
 import be.icc.Pid_Reservations_2024.Repositories.ShowRepository;
+import com.github.slugify.Slugify;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ShowService {
@@ -30,4 +38,86 @@ public class ShowService {
     public Show getShow(long id) {
         return showRepository.findById(id).orElse(null);
     }
+
+    public void add(Show show) {
+        showRepository.save(show);
+    }
+
+//    public void update(Long id, Show show) {
+//        showRepository.save(show);
+//    }
+//
+//
+//    public void delete(Long id) {
+//        //Long indice = Long.parseLong(id);
+//        Optional<Show> showOpt = showRepository.findById(indice);
+//
+//        if (showOpt.isPresent()) {
+//            Show show = showOpt.get();
+//
+//            // Nettoyer la relation ManyToMany avec ArtistType
+//            for (ArtisteType at : new ArrayList<>(show.getArtistTypes())) {
+//                show.removeArtistType(at);  // Cette méthode gère aussi le côté inverse
+//            }
+//
+//            showRepository.deleteById(indice.intValue());
+//        }
+//    }
+
+    public void update(Long id, Show newShowData) {
+        Optional<Show> optionalShow = showRepository.findById(id);
+        if (optionalShow.isPresent()) {
+            Show existingShow = optionalShow.get();
+
+            // Mettre à jour les champs modifiables
+            existingShow.setTitle(newShowData.getTitle());
+            existingShow.setPosterUrl(newShowData.getPosterUrl());
+            existingShow.setDuration(newShowData.getDuration());
+            existingShow.setBookable(newShowData.getBookable());
+            // Vous pouvez décider de mettre à jour la date si besoin (par exemple, une date de dernière modification)
+            // existingShow.setCreated_in(newShowData.getCreated_in());
+            existingShow.setLocation(newShowData.getLocation());
+
+            // Si le titre a changé, recalculer le slug
+            if (!existingShow.getTitle().equals(newShowData.getTitle())) {
+                try {
+                    Slugify slg = Slugify.builder().build();
+                    existingShow.setSlug(slg.slugify(newShowData.getTitle()));
+                } catch (Exception e) {
+                    // Gérez l'exception de slugification selon votre logique
+                    e.printStackTrace();
+                }
+            }
+
+            // Sauvegarder l'objet mis à jour
+            showRepository.save(existingShow);
+        } else {
+            throw new EntityNotFoundException("Show with id " + id + " not found.");
+        }
+    }
+
+    public void delete(Long id) {
+        Optional<Show> showOpt = showRepository.findById(id);
+        if (showOpt.isPresent()) {
+            Show show = showOpt.get();
+
+            // Nettoyer la relation ManyToMany avec ArtisteType.
+            // On crée une copie de la liste pour éviter une ConcurrentModificationException.
+            for (ArtisteType at : new ArrayList<>(show.getArtistTypes())) {
+                show.removeArtistType(at); // Assurez-vous que cette méthode retire correctement la relation du côté inverse
+            }
+
+            // Supprimer le show par son identifiant
+            showRepository.deleteById(id.intValue());
+
+        } else {
+            throw new EntityNotFoundException("Show with id " + id + " not found.");
+        }
+    }
+
+
+    public List<Show> getFromLocation(Location location) {
+        return showRepository.findByLocation(location);
+    }
+
 }
