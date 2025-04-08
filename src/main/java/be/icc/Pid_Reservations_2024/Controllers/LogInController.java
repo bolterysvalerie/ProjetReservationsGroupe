e412543cf915ce4b277287c3736d25e477b508e5
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-04-08 13:19:28 +0200

    Autres adaptations pour la visibilité des artistes...

--- src/main/java/be/icc/Pid_Reservations_2024/Controllers/LogInController.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/LogInController.java
@@ -1,5 +1,6 @@
 package be.icc.Pid_Reservations_2024.Controllers;
 
+import be.icc.Pid_Reservations_2024.Enums.UserRoles;
 import be.icc.Pid_Reservations_2024.Repositories.UserRepository;
 import be.icc.Pid_Reservations_2024.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +38,33 @@ public class LogInController {
         model.addAttribute("error", "Login ou mot de passe incorrect.");
         return "LogIn/LogIn";
     }
+
+//    // Traitement de la connexion
+//    @PostMapping("/LogIn")
+//    public String processLogin(@ModelAttribute("loginForm") User loginForm, Model model) {
+//        // Recherche de l'utilisateur en base de données
+//        User userFromDb = userRepository.findByLogin(loginForm.getLogin());
+//
+//        // Vérification utilisateur et mot de passe
+//        if (userFromDb != null && passwordEncoder.matches(loginForm.getPassword(), userFromDb.getPassword())) {
+//
+//            // Vérification si l'utilisateur est un administrateur
+//            if (UserRoles.ADMIN.equals(userFromDb.getRole())) {
+//                model.addAttribute("message", "Bienvenue, Administrateur !");
+//                System.out.println(userFromDb.getRole());
+//                return "Artist/index"; // Vue pour l'administrateur
+//            }
+//
+//            // Si ce n'est pas un administrateur
+//            model.addAttribute("message", "Bienvenue, " + userFromDb.getFirstName() + " !");
+//            System.out.println(userFromDb.getRole());
+//            return "Modification/ProfileModification"; // Vue pour les utilisateurs
+//        }
+//
+//        // Gestion des erreurs d'identification
+//        model.addAttribute("error", "Login ou mot de passe incorrect.");
+//        return "LogIn/LogIn";
+//    }
+
 
 }
