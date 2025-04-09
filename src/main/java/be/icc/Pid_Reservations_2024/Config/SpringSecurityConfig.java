9c3dce774565fbbfbbbd72a38d459fff566838a4
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2025-04-09 08:20:58 +0200

    merge fait

--- src/main/java/be/icc/Pid_Reservations_2024/Config/SpringSecurityConfig.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Config/SpringSecurityConfig.java
@@ -50,7 +50,7 @@ public class SpringSecurityConfig {
                         .deleteCookies("JSESSIONID"))
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(
-                                new LoginUrlAuthenticationEntryPoint("/LogIn n?loginRequired=true")))
+                                new LoginUrlAuthenticationEntryPoint("/LogIn?loginRequired=true")))
                 .build();
     }
 }
