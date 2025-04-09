5afa0419855b5cf5ffd96d4f6ed82646045ae7eb
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2025-04-09 08:08:42 +0200

    modification du spring security pour le bouton se deconecter

--- src/main/java/be/icc/Pid_Reservations_2024/Config/SpringSecurityConfig.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Config/SpringSecurityConfig.java
@@ -49,7 +49,7 @@ public class SpringSecurityConfig {
                         .deleteCookies("JSESSIONID"))
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(
-                                new LoginUrlAuthenticationEntryPoint("/LogIgit n?loginRequired=true")))
+                                new LoginUrlAuthenticationEntryPoint("/LogIn n?loginRequired=true")))
                 .build();
     }
 }
