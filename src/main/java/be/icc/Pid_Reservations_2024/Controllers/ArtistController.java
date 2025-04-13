c371e913ac93bf819af68db00619c70ce73387e5
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-04-13 04:51:15 +0200

    update pour le backoffice

--- src/main/java/be/icc/Pid_Reservations_2024/Controllers/ArtistController.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Controllers/ArtistController.java
@@ -2,6 +2,7 @@ package be.icc.Pid_Reservations_2024.Controllers;
 
 import be.icc.Pid_Reservations_2024.Models.Artist;
 import be.icc.Pid_Reservations_2024.Services.ArtistService;
+import be.icc.Pid_Reservations_2024.Services.TypeService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,9 @@ public class ArtistController {
     @Autowired
     ArtistService artistService;
 
+    @Autowired
+    private TypeService typeService; // Pour récupérer la liste des types
+
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/artists")
     public String index(Model model) {
@@ -36,7 +40,7 @@ public class ArtistController {
     public String show(@PathVariable("id") long id, Model model) {
         Artist artist = artistService.getArtist(id);
 
-        model.addAttribute("artist",artist);
+        model.addAttribute("artist", artist);
         model.addAttribute("title", "Profile of an artist");
 
         return "Artist/show";
@@ -49,7 +53,8 @@ public class ArtistController {
         if (!model.containsAttribute("artist")) {
             model.addAttribute("artist", new Artist());
         }
-
+        // Charger la liste des types disponibles dans le modèle.
+        model.addAttribute("types", typeService.getAllTypes());
         return "Artist/create";
     }
 
@@ -57,15 +62,17 @@ public class ArtistController {
     @PostMapping("/artist/create")
     public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model, RedirectAttributes redirAttrs) {
 
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             model.addAttribute("errorMessage", "Failure of the artist’s creation!");
+            // En cas d’erreur, il faut aussi recharger la liste des types
+            model.addAttribute("types", typeService.getAllTypes());
             return "Artist/create";
         }
 
         artistService.addArtist(artist);
         redirAttrs.addFlashAttribute("successMessage", "Successfully added artist!");
 
-        return "redirect:/artist/"+artist.getId();
+        return "redirect:/artist/" + artist.getId();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -73,15 +80,17 @@ public class ArtistController {
     public String edit(Model model, @PathVariable("id") long id, HttpServletRequest httpServletRequest) {
         Artist artist = artistService.getArtist(id);
 
-        model.addAttribute("artist",artist);
+        model.addAttribute("artist", artist);
+        // Charger la liste des types disponibles pour l’édition
+        model.addAttribute("types", typeService.getAllTypes());
 
         // Generate the return link for cancel
         String referrer = httpServletRequest.getHeader("Referer");
 
-        if(referrer != null && !referrer.equals("")) {
+        if (referrer != null && !referrer.equals("")) {
             model.addAttribute("back", referrer);
-        }else {
-            model.addAttribute("back", "/artist/"+artist.getId());
+        } else {
+            model.addAttribute("back", "/artist/" + artist.getId());
         }
 
         return "Artist/edit";
@@ -90,20 +99,21 @@ public class ArtistController {
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/artist/{id}/edit")
     public String update(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, @PathVariable("id") long id, Model model, RedirectAttributes redirAttrs) {
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
+
             return "Artist/edit";
         }
 
         Artist artistExisting = artistService.getArtist(id);
 
-        if(artistExisting == null) {
+        if (artistExisting == null) {
             return "Artist/index";
         }
         artistService.updateArtist(id, artist);
 
         redirAttrs.addFlashAttribute("successMessage", "Artist successfully modified");
 
-        return "redirect:/artist/"+artist.getId();
+        return "redirect:/artist/" + artist.getId();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -111,7 +121,7 @@ public class ArtistController {
     public String delete(@PathVariable("id") long id, Model model, RedirectAttributes redirAttrs) {
         Artist artistExisting = artistService.getArtist(id);
 
-        if(artistExisting != null) {
+        if (artistExisting != null) {
             artistService.deleteArtist(id);
             redirAttrs.addFlashAttribute("successMessage", "Artist successfully deleted!");
         } else {
