c13237bdf3734e1611578ddeefed9e058bf64d6c
Valerie Bolterys <bolterysvalerie1988@gmail.com>
2025-04-13 05:24:30 +0200

    ajout d'une méthode dans LocalityRepository pour faire un fetch join

--- src/main/java/be/icc/Pid_Reservations_2024/Repositories/LocalityRepository.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Repositories/LocalityRepository.java
@@ -1,12 +1,20 @@
 package be.icc.Pid_Reservations_2024.Repositories;
 
 import be.icc.Pid_Reservations_2024.Models.Locality;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface LocalityRepository extends CrudRepository<Locality, Long> {
 
     Locality findByPostalCode(String postalCode);
 
     Locality findByLocality(String locality);
+
+    // <-- Ajoute la méthode ci-dessous pour faire le fetch join
+    @Query("SELECT l FROM Locality l LEFT JOIN FETCH l.locations WHERE l.id = :id")
+    Optional<Locality> findByIdWithLocations(@Param("id") Long id);
 
 }
