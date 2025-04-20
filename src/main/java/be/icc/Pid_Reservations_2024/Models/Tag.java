c7e1f10f133183ff22b8bba2d68e68b88a9ff4bd
Brahms <bramso.brahms@gmail.com>
2025-04-20 21:00:39 +0200

    Create TagEntity with relation many to many

---
+++ src/main/java/be/icc/Pid_Reservations_2024/Models/Tag.java
@@ -0,0 +1,34 @@
+package be.icc.Pid_Reservations_2024.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Data @NoArgsConstructor
+@Entity
+@Getter @Setter
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "tag", unique = true, nullable = false, length = 30)
+    private String tag;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tag_shows",
+            joinColumns = @JoinColumn(name = "tag_id"),
+            inverseJoinColumns = @JoinColumn(name = "show_id")
+    )
+    List<Show> shows;
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "tag='" + tag + '\'' +
+                '}';
+    }
+}
