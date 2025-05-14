c33f11343bdd4575b595b0352a60a6d57bfb2162
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2025-05-14 18:57:29 +0200

    security

--- src/main/java/be/icc/Pid_Reservations_2024/Config/SpringSecurityConfig.java
+++ src/main/java/be/icc/Pid_Reservations_2024/Config/SpringSecurityConfig.java
@@ -36,8 +36,8 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
         return http.cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-//                .csrf(Customizer.withDefaults())
+//                .csrf(csrf -> csrf.disable())
+                .csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/admin").hasRole("ADMIN");
                     auth.requestMatchers("/user").hasRole("MEMBER");
